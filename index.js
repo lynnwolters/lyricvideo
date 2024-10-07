397d3b45057714d7bdd9821f08125df4b3024d63
Lynn Wolters <lynn.wolters27@gmail.com>
2024-10-07 17:00:07 +0200

    New

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
+// **************** //
+// checkScreenSize //
+// ************** //
 
-const words = document.querySelectorAll("main section:nth-of-type(1) h1 span");
-const tl = gsap.timeline({ repeat: -1, yoyo: true }); 
+let biggerThan = false; 
 
-const times = [1.4, 1.6, 2.5, 2.8, 3, 3.2, 3.5, 3.8]; 
+function checkScreenSize() {
+    if (window.innerWidth > 480) {
+        document.getElementById("check-screen-size").style.display = "flex";
+        biggerThan = true;
+    } else {
+        document.getElementById("check-screen-size").style.display = "none";
+        if (biggerThan) {
+            location.reload(); 
+        }
+    }
+}
 
-words.forEach((word, index) => {
-    tl.from(word, {
-        duration: 0.5,
-        opacity: 0,
-        y: 20,
-        ease: "power1.out",
-    }, times[index]); 
-});
+checkScreenSize();
 
-const items = document.querySelectorAll("main section:nth-of-type(2) div img");
+window.onresize = checkScreenSize;
 
-gsap.to(items, {
-    scaleY: 3, 
-    stagger: 0.1, 
-    duration: .3, 
-    ease: "power1.inOut", 
-    repeat: -1, 
-    yoyo: true, 
-});
+// ********************* //
+// textSectionAnimation //
+// ******************* //
 
-
+// ********************* //
+// wordSectionAnimation //
+// ******************* //
